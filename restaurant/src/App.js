d102130884ee2b15fcc51a4c0b0bf7c4c7673518
Facundo Monzon <fimonzon@mail.austral.edu.ar>
2021-10-05 19:10:27 -0300

    react3

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -5,12 +5,13 @@ import pgimg from './images/restaurant.jpg'
 function App() {
   return (
     <div className="App">
-        <div className="navegacion">
+      <div className="navegacion">
           <h4 className="nav-h4">Inicio</h4>
           <h4 className="nav-h4">Menus</h4>
           <h4 className="nav-h4">Contacto</h4>
           <h4 className="nav-h4">Trabaja con nosotros</h4>
-        </div>
+      </div>
+
         <div>
           <header className="encabezado">Restaurante Austral</header>
         
@@ -18,18 +19,14 @@ function App() {
         <div className="presentacion" >
           <h2 className="h2">Nuestros Productos</h2>
         <div className="presentacion2">
-        <div class="card">
-            <img src={pgimg} className="imagen1" alt="...."/>
+          <div class="card">
             <div class="card-body">
+            <img src={pgimg} className="imagen1" alt="...."/>
               <h5 class="card-title">Card title</h5>
               <p className="parrafo"> Este restaurante esta pensando para un menú universitario.
-                <br/>Te ofrecemos los diferentes tipos de menú,
+              <br/>Te ofrecemos los diferentes tipos de menú,
                     rápidos y deliciosos:
-                    </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-
-          <div className="presentacion3">
+              </p>
             <ul>
                        <li>Sadnwich</li>
                        <li>Empanadas</li>
@@ -39,20 +36,58 @@ function App() {
                        <li>Cafeteria</li>
                        <li>Licuados</li>
             </ul>
-        
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+         
           </div>
-</div>
-
-
-        
+          <div class="card">
+            <div class="card-body">
+            <img src={pgimg} className="imagen1" alt="...."/>
+              <h5 class="card-title">Card title</h5>
+              <p className="parrafo"> Este restaurante esta pensando para un menú universitario.
+              <br/>Te ofrecemos los diferentes tipos de menú,
+                    rápidos y deliciosos:
+              </p>
+            <ul>
+                       <li>Sadnwich</li>
+                       <li>Empanadas</li>
+                       <li>Carlitos</li>
+                       <li>Pizzas</li>
+                       <li>Bebidas frescas</li>
+                       <li>Cafeteria</li>
+                       <li>Licuados</li>
+            </ul>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+         
+          </div>
+          <div class="card">
+            <div class="card-body">
+            <img src={pgimg} className="imagen1" alt="...."/>
+              <h5 class="card-title">Card title</h5>
+              <p className="parrafo"> Este restaurante esta pensando para un menú universitario.
+              <br/>Te ofrecemos los diferentes tipos de menú,
+                    rápidos y deliciosos:
+              </p>
+            <ul>
+                       <li>Sadnwich</li>
+                       <li>Empanadas</li>
+                       <li>Carlitos</li>
+                       <li>Pizzas</li>
+                       <li>Bebidas frescas</li>
+                       <li>Cafeteria</li>
+                       <li>Licuados</li>
+            </ul>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+         
+          </div>
         </div>
-        <h4 className="nav2-h4">Pedidos y encargues</h4>
-
 
     </div>
-    
+  </div>
 
-    </div>
+
   );
 }
 
---
+++ restaurant/src/components/Navbar/Navbar.js
@@ -0,0 +1,6 @@
+<div className="navegacion">
+          <h4 className="nav-h4">Inicio</h4>
+          <h4 className="nav-h4">Menus</h4>
+          <h4 className="nav-h4">Contacto</h4>
+          <h4 className="nav-h4">Trabaja con nosotros</h4>
+        </div>
